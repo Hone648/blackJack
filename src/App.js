8a2a3192d6e79f4de7889d1ee213e2d7cb942530
Hunter-Kam <hone648@gmail.com>
2021-03-16 18:25:23 -0700

    created splash page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import "./App.css";
 import NewGame from "./views/NewGame";
+import { Router, navigate } from "@reach/router";
 
 function App() {
   return (
     <div className="App">
-      <div className="ui grid container">
-        <div className="five wide column"></div>
-        <div className="six wide column">
-          <div className="header">
-            <h2 class="ui large center aligned blue icon header">
-              <i class="circular wheelchair blue icon"></i>
-              <p className="header-content">BlackJack</p>
-            </h2>
-          </div>
-        </div>
-        <div className="five wide column"></div>
-        <div className="play-area">
-          <NewGame />
-        </div>
-      </div>
+      <NewGame />
     </div>
   );
 }
--- src/views/NewGame.js
+++ src/views/NewGame.js
@@ -4,9 +4,23 @@ import "./NewGame.css";
 const NewGame = () => {
   return (
     <div>
-      <button className="ui blue button">
-        <p className="button-content">New Game</p>
-      </button>
+      <div className="ui centered grid container">
+        <div className="five wide column"></div>
+        <div className="six wide column">
+          <div className="header">
+            <h2 class="ui large center aligned blue icon header">
+              <i class="circular wheelchair blue icon"></i>
+              <p>BlackJack</p>
+            </h2>
+          </div>
+        </div>
+        <div className="five wide column"></div>
+        <div className="row">
+          <div className="two wide centered column">
+            <button className="ui blue button">New Game</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
