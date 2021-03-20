d8c4c5684dd312cd707611c2ccf9ac3cfaf2e48c
Hunter-Kam <hone648@gmail.com>
2021-03-19 22:27:12 -0700

    aligned cards on table.js

--- src/views/Table/Table.js
+++ src/views/Table/Table.js
@@ -1,31 +1,40 @@
 import React from "react";
 import background from "./images/table.jpeg";
 
+const cardHeight = 24;
+const cardWidth = cardHeight * 0.7142;
+
+const myStyles = {
+  container: {
+    backgroundImage: `url(${background})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "103vh",
+    width: "100vw",
+  },
+  cardContainer: {
+    marginTop: "20vh",
+  },
+  card: {
+    margin: "5px",
+    height: `${cardHeight}vh`,
+    width: `${cardWidth}vh`,
+    backgroundColor: "green",
+  },
+};
+
 const Table = () => {
-  const myStyles = {
-    container: {
-      backgroundImage: `url(${background})`,
-      backgroundPosition: "center",
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-      height: "100vh",
-      width: "100vw",
-    },
-    card: {
-      height: "400px",
-      textAlign: "center",
-    },
-  };
   return (
     <div>
       <div style={myStyles.container}>
-        <div class="ui centered grid">
-          <div style={myStyles.card} className="three wide red column">
-            <div>Card 1</div>
+        <div class="ui stackable centered relaxed grid">
+          <div style={myStyles.cardContainer} className="two wide column">
+            <div style={myStyles.card}>Card 1 XXXXXXXXXXXX</div>
           </div>
           <div className="one wide column"></div>
-          <div style={myStyles.card} className="four wide blue column">
-            Card 2
+          <div style={myStyles.cardContainer} className="two wide column">
+            <div style={myStyles.card}>card 2 XXXXXXXXXXXX</div>
           </div>
         </div>
       </div>
